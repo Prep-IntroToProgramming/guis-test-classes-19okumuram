8e9565180d7eeaae5ccc478f9efd41ca3b43850c
Mei-Li Okumura <19okumuram@flintridgeprep.org>
2019-02-07 09:22:56 -0800

    hehe

--- SimpleGui1.java
+++ SimpleGui1.java
@@ -2,18 +2,17 @@ import javax.swing.*;
 
 public class SimpleGui1{ 
     public SimpleGui1() {
-        JFrame frame = new JFrame();
-        JButton button = new JButton("click me");
-        //JLabel label = new JLabel("Swing is pretty cool");
-
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(button);
-        //frame.add(label);
-        frame.setSize(300, 300);
-        frame.setVisible(true);
+        JFrame frame = new JFrame(); //creates a new window
+        JButton button = new JButton("click me"); //creating new JButton
+        JLabel label = new JLabel("Swing is pretty cool"); //creates new JLabel object
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //
+        frame.add(button); //adds the button to the frame (makes it appear)
+        frame.add(label); //adds to the frame
+        frame.setSize(300, 300); //sets window to a 300x300 pixel? square
+        frame.setVisible(true); //makes it appear
     }
 
     public static void main(String[] args) {
-        SimpleGui1 gui = new SimpleGui1();
+        SimpleGui1 gui = new SimpleGui1(); //creates new SimpleGui1 object using constructor
     }
 } 
--- SimpleGui1B.java
+++ SimpleGui1B.java
@@ -4,26 +4,41 @@ import java.awt.event.*;
 public class SimpleGui1B implements ActionListener{ 
     JFrame frame;
     JButton button;
-    
+    int numClicks = 0;
     public SimpleGui1B() {
-        button = new JButton("click me");
-        frame = new JFrame();
+        button = new JButton("click me"); //create new button
+        frame = new JFrame(); //new frame
 
         //JLabel label = new JLabel("Swing is pretty cool");
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(button);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //ensures program stops running when window is closed
+        frame.add(button); //add button
         //frame.add(label);
-        
-        button.addActionListener(this);
-        frame.setSize(300, 300);
-        frame.setVisible(true);
+
+        button.addActionListener(this); //action listener superclass; tells program when to react to button
+        frame.setSize(300, 300); //sets frame size
+        frame.setVisible(true); //makes frame visible
     }
 
     public void actionPerformed(ActionEvent event) {
-        button.setText("I've been clicked!");
+        numClicks++;
+        if (numClicks == 1) {
+            button.setText("I've been clicked " + numClicks + " time!"); //invoked when action (click) happens
+        } else if (numClicks > 1) {
+            if (numClicks == 13) {
+                button.setText(numClicks + " is an unlucky number rip fam");
+            } else if (numClicks == 50) {
+                button.setText(numClicks + " means we're halfway to 100");
+            } else if (numClicks == 73) {
+                button.setText(numClicks + "??? how bored are you?");
+            } else if (numClicks == 100) {
+                button.setText(numClicks + "!! yay! you must be really bored");
+            } else {
+                button.setText("I've been clicked " + numClicks + " times!");
+            }
+        }
     }
-    
+
     public static void main(String[] args) {
         SimpleGui1B gui = new SimpleGui1B();
     }
